5e6b9775b6a5e6c369cf2f5d254cdbbb966f9709
webklex <info@webklex.com>
2022-12-13 04:56:42 +0100

    thread and created_at accessor added

--- scraper/cache.go
+++ scraper/cache.go
@@ -1,8 +1,43 @@
 package scraper
 
+import (
+	"time"
+)
+
 type CachedTweet struct {
-	Index        int                  `json:"index"`
+	Version      string               `json:"version"`
 	User         UserResult           `json:"user"`
 	Tweet        TweetResult          `json:"tweet"`
 	Conversation ConversationResponse `json:"conversation"`
+
+	createdAt time.Time
+}
+
+type ThreadItem struct {
+	Tweet TweetResult
+	User  ConversationUser
+}
+
+func (ct *CachedTweet) CreatedAt() time.Time {
+	if ct.createdAt.IsZero() {
+		ct.createdAt, _ = time.Parse("Mon Jan 02 15:04:05 -0700 2006", ct.Tweet.CreatedAt)
+	}
+	return ct.createdAt
+}
+
+func (ct *CachedTweet) Thread() map[string]*ThreadItem {
+	thread := map[string]*ThreadItem{}
+	for tweetId, tweet := range ct.Conversation.GlobalObjects.Tweets {
+		user, ok := ct.Conversation.GlobalObjects.Users[tweet.UserIdStr]
+		if !ok {
+			user = ConversationUser{}
+		}
+
+		thread[tweetId] = &ThreadItem{
+			Tweet: tweet,
+			User:  user,
+		}
+	}
+
+	return thread
 }
