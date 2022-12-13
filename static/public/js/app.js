92a5b37656ac197fae88e6f305995c156ffb444a
webklex <info@webklex.com>
2022-12-13 04:55:36 +0100

    Styles updated

--- static/public/js/app.js
+++ static/public/js/app.js
@@ -1,158 +1,91 @@
 // Main Application
+
+/**
+ * Top navigation (Desktop & Mobile)
+ * @type {HTMLElement}
+ */
+const menu = document.getElementById('menu');
+function toggleMenu() {
+    menu.classList.toggle('hidden');
+    menu.classList.toggle('w-full');
+    menu.classList.toggle('h-screen');
+}
+const items = menu.getElementsByTagName("li");
+for (let i = 0; i < items.length; i++) {
+    const link = items[i].getElementsByTagName("a")[0];
+    const path = link.getAttribute("href").split("?")[0];
+    if (path === location.pathname) {
+        link.classList.add('text-yellow-500', 'font-bold', 'opacity-100');
+        link.classList.remove('opacity-70');
+    }else{
+        link.classList.remove('text-yellow-500', 'font-bold', 'opacity-70')
+        link.classList.add('opacity-100');
+    }
+}
+
 (function() {
-    const errorHolder = document.getElementById("error-holder");
-    const tweetHolder = document.getElementById("tweet-holder");
-    const searchHolder = document.getElementById("search-holder");
-    const counterHolder = document.getElementById("counter-holder");
-    const loading = document.getElementById("loading");
+    const notificationHolder = document.getElementById("notification-holder");
+    const socket = new WebSocket(`ws://${location.host}/ws`);
 
-    fetch("/state").then(body => body.json()).then((resp => {
-        const socket = new WebSocket(`ws://${location.host}/ws`);
-        const mode = resp.mode
-        let counter = 0;
+    function truncate(str, n){
+        return (str.length > n) ? str.slice(0, n-1) + '&hellip;' : str;
+    }
 
-        // Display a given error message
-        const setError = (err) => {
-            errorHolder.innerHTML = `<div class='py-2 px-2 border-l-4 border-red-700'>An error occurred: ${err}</div>`
+    // Display a new tweet in the first position
+    const addTweet = (tweet) => {
+        notificationHolder.innerHTML = ""
+        const tdiv = document.createElement("div")
+        tdiv.classList.add("w-full", "border", "border-solid","border-slate-600","mb-4", "bg-slate-900", "clickable")
+
+        tdiv.innerHTML = `<div class="w-full border-l-4 border-solid border-green-500 bg-slate-900 py-2 px-4">
+            <h4 class="font-bold">New tweet fetched</h4>
+            <p>
+                ${truncate(tweet.full_text, 128)}
+            </p>
+        </div>`
+        tdiv.addEventListener("click", e => {
+            tdiv.remove();
+        })
+        notificationHolder.insertBefore(tdiv, notificationHolder.firstChild);
+        setTimeout(() => tdiv.remove(), 6000);
+    }
+
+    socket.onopen = function(e) {};
+
+    // Check if the websocket got closed correctly
+    socket.onclose = function(event) {
+        if (event.wasClean === false) {
+            // e.g. server process killed or network down
+            // event.code is usually 1006 in this case
+            console.log('[close] Connection died');
         }
+    };
 
-        const updateCounter = () => {
-            counterHolder.innerHTML = `<div class='py-2'>Tweets found: ${counter}</div>`
+    // Handle Websocket errors
+    socket.onerror = function(error) {};
+
+    // Handle all incoming messages
+    socket.onmessage = function(event) {
+        try {
+            const response = JSON.parse(event.data);
+            const keys = Object.keys(response.data);
+            const data = response.data;
+
+            if (response.errors.length > 0) {
+                return
+            }
+
+            for (let i = 0; i < keys.length; i++) {
+                switch (keys[i]) {
+                    case "tweet":
+                        return addTweet(data.tweet)
+                    default:
+                        console.log("response key not implemented:", keys[i])
+
+                }
+            }
+        }catch (e) {
+            console.log(e)
         }
-
-        // Display a new tweet in the first position
-        const addTweet = (user, tweet, conversation) => {
-            const threadLength = Object.keys(conversation.globalObjects.tweets).length;
-            let content = tweet.full_text;
-            tweet.entities.hashtags.map(ht => {
-                content = content.replace(new RegExp(`#${ht.text}( |$)`), `<a class="text-teal-500" href="https://twitter.com/hashtag/${ht.text}" target="_blank" rel="noreferrer">#${ht.text}</a> `)
-            }).join(" ")
-            tweet.entities.user_mentions.map(ht => {
-                content = content.replace(new RegExp(`@${ht.screen_name}( |$)`), `<a class="text-teal-600" href="https://twitter.com/${ht.screen_name}" target="_blank" rel="noreferrer">@${ht.screen_name}</a> `)
-            }).join(" ")
-            tweet.entities.urls.map(ht => {
-                content = content.replace(`${ht.url}`, `<a class="text-yellow-600" href="${ht.expanded_url}" target="_blank" rel="noreferrer">${ht.expanded_url}</a> `)
-            }).join(" ")
-            tweet.entities.media?.map(ht => {
-                content = content.replace(`${ht.url}`, ``)
-            })
-            const createdAt = new Date(tweet.created_at);
-            const tweetDate = `${createdAt.getFullYear()}.${("0"+(createdAt.getMonth()+1)).slice(-2)}.${("0"+createdAt.getDate()).slice(-2)} ${("0" + createdAt.getHours()).slice(-2)}:${("0" + createdAt.getMinutes()).slice(-2)}:${("0" + createdAt.getSeconds()).slice(-2)}`
-
-            const tdiv = document.createElement("div")
-            tdiv.classList.add("w-full", "md:w-2/6", "xl:w-1/4","py-2","px-2")
-
-            tdiv.innerHTML = `
-<div class="border border-solid border-1 border-slate-600 py-2 px-2 flex flex-wrap rounded">
-    <div class="w-auto pr-2">
-        <a href="https://twitter.com/${user.legacy.screen_name}" target="_blank" rel="noreferrer">
-            <img class="rounded-full" src="/media/${user.rest_id}"  alt=""/>
-        </a> 
-    </div>
-    <div class="grow">
-        <a href="https://twitter.com/${user.legacy.screen_name}" class="break-words" target="_blank" rel="noreferrer">
-            <span>${user.legacy.name}</span>
-            <span class="text-xs text-slate-400">
-                <br />
-                @${user.legacy.screen_name}
-            </span>
-        </a>
-    </div>
-    <div class="w-full pt-2 break-words">
-        ${content}
-    </div>
-    <div class="w-full">
-        ${conversation.globalObjects.tweets?.[tweet.id_str]?.extended_entities.media?.map(ht => {
-            let url = ht.type === "video" ? ht.video_info?.variants[ht.video_info.variants.length - 1]?.url : ht.media_url_https;
-            
-            if (mode === "offline") {
-                url = ht.type === "video" ? `/video/${ht.id_str}` : `/media/${ht.id_str}`;
-            }
-            return `<a href="${url}" target="_blank" rel="noreferrer"><img class="rounded pt-2" src="/media/${ht.id_str}" rel="noreferrer" alt=""/></a>`;
-        })?.join(" ") ?? ""}
-    </div>
-    ${threadLength > 1 ? `<div class="w-full pt-2"><a href="/thread/${tweet.id_str}" class="text-teal-600" target="_blank" rel="noreferrer">🧵 thread (${threadLength})</a></div>` : ""}
-    <div class="w-55/100 text-xs text-slate-400 pt-2" title="Tweet ID">
-        <a href="https://twitter.com/${user.legacy.screen_name}/status/${tweet.id_str}" class="text-yellow-600" target="_blank" rel="noreferrer">🐦 ${tweet.id_str}</a>
-    </div>
-    <div class="w-45/100 text-xs text-right text-slate-400 pt-2">
-        ${tweetDate}
-    </div>
-</div>`
-            tweetHolder.insertBefore(tdiv, tweetHolder.firstChild);
-        }
-
-        // Register an event listener on the search input field
-        searchHolder.addEventListener('change', function(e) {
-            socket.send(JSON.stringify({
-                command: "search_tweets",
-                payload: {
-                    query: e.target.value
-                }
-            }));
-        }, false);
-
-        // Get all tweets if the websocket connection has been established and opened
-        socket.onopen = function(e) {
-            socket.send(JSON.stringify({
-                command: "get_tweets"
-            }));
-        };
-
-        // Check if the websocket got closed correctly
-        socket.onclose = function(event) {
-            if (event.wasClean === false) {
-                // e.g. server process killed or network down
-                // event.code is usually 1006 in this case
-                console.log('[close] Connection died');
-            }
-        };
-
-        // Handle Websocket errors
-        socket.onerror = function(error) {
-            setError(error.message)
-        };
-
-        // Handle all incoming messages
-        socket.onmessage = function(event) {
-            loading.classList.remove("block")
-            loading.classList.add("hidden")
-            try {
-                const response = JSON.parse(event.data);
-                const keys = Object.keys(response.data);
-                const data = response.data;
-
-                if (response.errors.length > 0) {
-                    setError(data.errors.join(", "));
-                    return
-                }
-
-                for (let i = 0; i < keys.length; i++) {
-                    switch (keys[i]) {
-                        case "tweet":
-                            counter++;
-                            updateCounter();
-                            return addTweet(data.user, data.tweet, data.conversation)
-                        case "tweets":
-                            counter = 0;
-                            if (data["tweets"].length === 0) {
-                                return tweetHolder.innerHTML = "<div class='w-full text-center pt-8 pb-4'>Not tweets found..</div>";
-                            }
-                            tweetHolder.innerHTML = "";
-                            updateCounter();
-                            data["tweets"].map(tweet => {
-                                counter++;
-                                return addTweet(tweet.user, tweet.tweet, tweet.conversation)
-                            });
-                            return updateCounter();
-                        default:
-                            console.log("response key not implemented:", keys[i])
-
-                    }
-                }
-            }catch (e) {
-                console.log(e)
-            }
-        };
-    }))
+    };
 })();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 const colors = require('tailwindcss/colors');
+const copydir = require('copy-dir');
 
 delete colors['lightBlue'];
 delete colors['warmGray'];
 delete colors['trueGray'];
 delete colors['coolGray'];
 delete colors['blueGray'];
+
+// node_modules/@fortawesome/fontawesome-free/webfonts
+copydir.sync('node_modules/@fortawesome/fontawesome-free/webfonts', 'static/public/webfonts', {
+    utimes: true,  // keep add time and modify time
+    mode: true,    // keep file mode
+    cover: true    // cover file when exists, default is true
+});
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -20,4 +28,7 @@ module.exports = {
         extend: {},
     },
     plugins: [],
+    safelist: [
+        "text-teal-500"
+    ]
 }
